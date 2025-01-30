831bdf08d07e7c9a812b2ec8bd4ad6e950cf4508
venkatsai963 <venkatgunji298@gmail.com>
2025-01-30 23:48:25 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,9 @@
+public class Logical {
+    public static void main(String[] args) {
+        int a=3;
+        int b=165;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+    }
+}
